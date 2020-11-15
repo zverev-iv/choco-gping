93de0fdcadeab899620572c2e58a17aa078e35f9
Илья Зверев <izverev@logrocon.com>
2020-11-16 00:09:15 +0300

    init

---
+++ gping/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$meta = Get-Content -Path $env:ChocolateyPackageFolder\tools\packageArgs.json -Raw
+$packageArgs = @{}
+(ConvertFrom-Json $meta).psobject.properties | ForEach-Object { $packageArgs[$_.Name] = $_.Value }
+
+$packageArgs["packageName"] = $env:ChocolateyPackageName
+$packageArgs["unzipLocation"] = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyZipPackage @packageArgs
