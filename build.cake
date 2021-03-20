1557b6285bd4f75f082ce01918baad68e33d5dc7
zverev iv <mail@zverev-iv.me>
2021-03-20 13:11:46 +0300

    up 1.0.2

--- build.cake
+++ build.cake
@@ -3,7 +3,7 @@ var target = Argument("target", "Publish");
 var packageInfo = new ChocolateyPackSettings {
     //PACKAGE SPECIFIC SECTION
     Id                       = "gping",
-    Version                  = "1.0.1",
+    Version                  = "1.2.0",
     PackageSourceUrl         = new Uri("https://github.com/zverev-iv/choco-gping"),
     Owners                   = new[] {"zverev-iv"},
     //SOFTWARE SPECIFIC SECTION
--- src/chocolateyinstall.ps1
+++ src/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $packageArgs = @{
 	packageName    = $env:ChocolateyPackageName
 	unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 	softwareName   = "gping"
-	url64bit       = "https://github.com/orf/gping/releases/download/v1.0.1/gping-Windows-x86_64.zip";
-	checksum64     = "824FE3BA3A800F38A8D20BEB0BB4F0CE46FEA957D4501750AA2F9E7CF015BC86";
+	url64bit       = "https://github.com/orf/gping/releases/download/v1.2.0/gping-Windows-x86_64.zip";
+	checksum64     = "604CE645CD4F5D68A4B6E0423C1E5E81C7400D0C7AC85C3834A9FB2778482FC3";
 	checksumType64 = "sha256";
 }
 
