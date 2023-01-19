56bea011ebe5f3750b3fe33c5917c93a397996c0
zverev iv <mail@zverev-iv.me>
2023-01-19 19:36:56 +0300

    new pipeline

--- src/chocolateyinstall.ps1
+++ src/chocolateyinstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
 	packageName    = $env:ChocolateyPackageName
 	unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-	softwareName   = "gping"
-	url64bit       = "https://github.com/orf/gping/releases/download/gping-v1.2.7/gping-Windows-x86_64.zip";
-	checksum64     = "92d0ebb50cbebb0b187e1d4ceba054813ca4e2825ad2db3e6633b6c60a51d8fe";
-	checksumType64 = "sha256";
+	softwareName  = "${softwareName}"
+	url64bit      = "${url64bit}"
+	checksum64    = "${checksum64}"
+	checksumType64= "${checksumType64}"
 }
 
 Install-ChocolateyZipPackage @packageArgs
